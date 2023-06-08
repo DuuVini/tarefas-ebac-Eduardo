190a38fbab35845922f4494eaa8a8e44dea2e63a
DuuVini <evoliveira.invest@gmail.com>
2023-06-08 09:42:19 -0300

    Programa que calcula média dos alunos

---
+++ mod8/notas-calc-media/src/Notas.java
@@ -0,0 +1,14 @@
+public class Notas {
+    public static (String args[]){
+
+    public static void Notas{
+        int nota1 =10;
+        int nota2=5;
+        int nota3=7;
+        int nota4=8;
+        int media=(nota1+nota2+nota3+nota4)/4;
+
+        System.out.println("Mé1dia do aluno é: "+media);
+    }
+}
+}
