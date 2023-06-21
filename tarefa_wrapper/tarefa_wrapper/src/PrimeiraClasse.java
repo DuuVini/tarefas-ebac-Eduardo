0dbc7acd8eee18620205fb34d1c95403f3c5d3b6
Eduardo Vinicius <evoliveira.invest@gmail.com>
2023-06-21 20:42:48 -0300

    comittando tarefa wrapper

---
+++ tarefa_wrapper/tarefa_wrapper/src/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class  PrimeiraClasse {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(num);
+        Integer num2 = num;
+        System.out.println(num2);
+        }
+}
